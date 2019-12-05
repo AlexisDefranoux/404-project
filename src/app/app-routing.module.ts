a8ed37f4a91f29a923de2442b27897729f621d2c
Alexis Defranoux <alexis.defranoux@etu.univ-cotedazur.fr>
2019-12-05 19:38:46 +0100

    add pretty structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {Test1Component} from './components/404-pages/test1/test1.component';
+import {HomeComponent} from './components/escape-game/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: Test1Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Test1Component } from './components/404-pages/test1/test1.component';
+import { HomeComponent } from './components/escape-game/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Test1Component
+    Test1Component,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
