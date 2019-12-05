5c30fba9264a542e51a34a8a96ab0994af5475bb
Alexis Defranoux <alexis.defranoux@etu.univ-cotedazur.fr>
2019-12-05 19:10:51 +0100

    setup project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Test1Component } from './components/404-pages/test1/test1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Test1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
